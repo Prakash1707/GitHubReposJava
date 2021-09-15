c7a6e1d3f524f0859c3938d3291f44634ef97ef4
Prakash1707 <saiprakashbaba1@gmail.com>
2021-09-15 15:49:36 +0530

    Created Customer Class

---
+++ GitHubRepositoryJava2/src/com/lti/demo/Customer.java
@@ -0,0 +1,14 @@
+package com.lti.demo;
+
+public class Customer {
+	private int id;
+	private String name;
+	private int bill;
+	public Customer(int id, String name, int bill) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.bill = bill;
+	}
+	
+}

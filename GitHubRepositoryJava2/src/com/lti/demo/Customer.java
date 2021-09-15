c0c25015f6be16d80c25a8af54829a335d745bf9
Prakash1707 <saiprakashbaba1@gmail.com>
2021-09-15 15:50:52 +0530

    Created toString method

--- GitHubRepositoryJava2/src/com/lti/demo/Customer.java
+++ GitHubRepositoryJava2/src/com/lti/demo/Customer.java
@@ -10,5 +10,27 @@ public class Customer {
 		this.name = name;
 		this.bill = bill;
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBill() {
+		return bill;
+	}
+	public void setBill(int bill) {
+		this.bill = bill;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", bill=" + bill + "]";
+	}
 	
 }
